cbda78666142ac5395b274fdd4092114dd19f239
favdagic1 <favdagic1@etf.unsa.ba>
2025-09-15 00:44:56 +0200

    Sidebar menu i Hero sekcija

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,10 +1,17 @@
 import trazi_ikona from './ikone/trazi.png'; 
 import user_ikona from './ikone/user.png';
 import cart_ikona from './ikone/cart.png';
+import menu_ikona from './ikone/menu.png';
+import hero_ikona from './logo/hero.jpg';
+
+
+import logo from './logo/trila-high-resolution-logo-grayscale-transparent.png';
 
 export const assets = {
-  logo: '/vite.svg', // Privremeno koristite postojeći Vite logo
+  logo: logo,
   trazi: trazi_ikona,
   user: user_ikona,
   cart: cart_ikona,
+  menu: menu_ikona,
+  hero: hero_ikona,
 };
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,14 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {assets} from '../assets/assets'
 import { Link, NavLink } from 'react-router-dom'
 const Navbar = () => {
+
+  const [visible,setVisible] = useState(false);
+
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
 
         <img src={assets.logo}  className='w-36' alt="" />
 
-        <ul className='hidden sm:flex gap-5 text-sm text-gray-700 hidden'>
-            <NavLink to='/home' className='flex flex-col items-center gap-1'> 
+        <ul className=' sm:flex gap-5 text-sm text-gray-700 hidden'>
+            <NavLink to='/' className='flex flex-col items-center gap-1'> 
                 <p>HOME</p>
                 <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/> 
             </NavLink>
@@ -50,9 +53,29 @@ const Navbar = () => {
 
             <Link to='/cart' className='relative'>
               <img src={assets.cart} className='w-5 min-w-5' alt="" />
-              <p className='absolite right-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>10</p>
+              <p className='absolute right-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>10</p>
             </Link>
+          <img onClick={()=>setVisible(true)} src={assets.menu} className='w-5 cursor-pointer sm:hidden' alt="" />
+
       </div>
+
+      {/* sidebar menu za manji ekran */}
+      <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full' : 'w-0'} sm:hidden`}>
+
+        <div className='flex flex-col text.gray-600'>
+          <div onClick={()=>setVisible(false)} className='flex items-center gap-4 p-3 cursor-pointer'>
+            <img className='h-4 rotate-180' src={assets.dropdown_icon} alt=""/>
+            <p>Back</p> 
+          </div>
+          <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/'>HOME</NavLink>
+          <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/collection'>COLLECTION</NavLink>
+          <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/about'>ABOUT</NavLink>
+          <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/contact'>CONTACT</NavLink>
+        </div>
+
+      </div>
+
+
     </div>
   )
 }
